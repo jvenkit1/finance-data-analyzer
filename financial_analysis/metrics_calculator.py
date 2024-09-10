6552edb9126b47ba23403c5033dcaa32a73f075c
Jairam Venkit <jairam0@gmail.com>
2024-09-09 21:33:18 -0700

    added new metrics to track

--- financial_analysis/metrics_calculator.py
+++ financial_analysis/metrics_calculator.py
@@ -7,6 +7,7 @@ class MetricsCalculator:
         self.cashflow = cashflow
         self.info = info
 
+    # Helper method to retrieve balance sheet values using multiple possible keys
     def _get_balance_sheet_value(self, keys):
         for key in keys:
             if key in self.balance_sheet.index:
@@ -14,6 +15,7 @@ class MetricsCalculator:
                     return self.balance_sheet.loc[key].iloc[0]
         return None
 
+    # Helper method to retrieve financial values using multiple possible keys
     def _get_financials_value(self, keys):
         for key in keys:
             if key in self.financials.index:
@@ -21,211 +23,185 @@ class MetricsCalculator:
                     return self.financials.loc[key].iloc[0]
         return None
 
+    # Industry, Sector, Short Name retrieval
     def get_industry(self):
-        """Retrieve company industry."""
         return self.info.get('industry', None)
 
     def get_sector(self):
-        """Retrieve company sector."""
         return self.info.get('sector', None)
 
     def get_short_name(self):
-        """Retrieve company short name."""
         return self.info.get('shortName', None)
 
-    # New: Stock Price, PE Ratio, EPS
+    # Stock Price, PE Ratio, EPS retrieval
     def get_stock_price(self):
-        """Retrieve current stock price."""
         return self.info.get('currentPrice', None)
 
     def calculate_pe_ratio(self):
-        """Calculate PE Ratio."""
         return self.info.get('trailingPE', None)
 
     def get_eps(self):
-        """Retrieve EPS (Earnings per Share)."""
         return self.info.get('trailingEps', None)
 
-    # Debt-to-Equity Ratio (DE Ratio)
+    # Debt-to-Equity Ratio (DE Ratio) using Total Liabilities and Common Stock Equity
     def calculate_de_ratio(self):
-        """Calculate Debt to Equity Ratio."""
-        total_liabilities = self._get_balance_sheet_value(['Total Liabilities Net Minority Interest', 'Total Liab'])
-        total_stockholder_equity = self._get_balance_sheet_value(['Common Stock Equity', 'Total Stockholder Equity'])
-
+        total_liabilities = self._get_balance_sheet_value(['Total Liabilities Net Minority Interest', 'Total Liabilities'])
+        total_stockholder_equity = self._get_balance_sheet_value(['Common Stock Equity', 'Stockholders Equity'])
         if total_liabilities is None or total_stockholder_equity is None:
             return None
         return total_liabilities / total_stockholder_equity
 
-    # Return on Equity (ROE)
+    # Return on Equity (ROE) using Net Income and Stockholder Equity
     def calculate_roe(self):
-        """Calculate Return on Equity."""
         net_income = self._get_financials_value(['Net Income'])
-        total_stockholder_equity = self._get_balance_sheet_value(['Common Stock Equity', 'Total Stockholder Equity'])
-
+        total_stockholder_equity = self._get_balance_sheet_value(['Common Stock Equity', 'Stockholders Equity'])
         if net_income is None or total_stockholder_equity is None:
             return None
         return net_income / total_stockholder_equity
 
     # Earnings Yield = Net Income / Market Cap
     def calculate_earnings_yield(self):
-        """Calculate Earnings Yield."""
         net_income = self._get_financials_value(['Net Income'])
         market_cap = self.info.get('marketCap', None)
-
         if net_income is None or market_cap is None:
             return None
         return net_income / market_cap
 
-    # Dividend Yield
+    # Dividend Yield from info
     def calculate_dividend_yield(self):
-        """Calculate Dividend Yield."""
         return self.info.get('dividendYield', None)
 
-    # Current Ratio
+    # Current Ratio using Total Current Assets and Total Current Liabilities
     def calculate_current_ratio(self):
-        """Calculate Current Ratio."""
-        current_assets = self._get_balance_sheet_value(['Total Current Assets'])
-        current_liabilities = self._get_balance_sheet_value(['Total Current Liabilities'])
-
+        current_assets = self._get_balance_sheet_value(['Current Assets'])
+        current_liabilities = self._get_balance_sheet_value(['Current Liabilities'])
         if current_assets is None or current_liabilities is None:
             return None
         return current_assets / current_liabilities
 
-    # PE to Growth (PEG Ratio)
+    # PE to Growth (PEG Ratio) from info
     def calculate_pe_to_growth(self):
-        """Calculate Price/Earnings to Growth (PEG) ratio."""
         return self.info.get('pegRatio', None)
 
-    # Price to Book Ratio
+    # Price to Book Ratio from info
     def calculate_price_to_book(self):
-        """Calculate Price to Book Ratio."""
         return self.info.get('priceToBook', None)
 
-    # Price-to-Sales (P/S) Ratio
+    # Price-to-Sales (P/S) ratio using Market Cap and Revenue
     def calculate_price_to_sales(self):
-        """Calculate Price-to-Sales (P/S) ratio."""
         market_cap = self.info.get('marketCap', None)
         revenue = self.get_revenue()
-
         if market_cap is None or revenue is None:
             return None
         return market_cap / revenue
 
-    # Enterprise Value to EBITDA (EV/EBITDA)
+    # EV/EBITDA ratio using Enterprise Value and EBITDA
     def calculate_ev_to_ebitda(self):
-        """Calculate Enterprise Value to EBITDA (EV/EBITDA)."""
         enterprise_value = self.info.get('enterpriseValue', None)
         ebitda = self._get_financials_value(['EBITDA'])
-
         if enterprise_value is None or ebitda is None:
             return None
         return enterprise_value / ebitda
 
-    # Price to Free Cash Flow (P/FCF)
+    # Price to Free Cash Flow (P/FCF) using Market Cap and Free Cash Flow
     def calculate_price_to_free_cash_flow(self):
-        """Calculate Price-to-Free Cash Flow (P/FCF) ratio."""
         market_cap = self.info.get('marketCap', None)
         free_cash_flow = self._get_financials_value(['Free Cash Flow'])
-
         if market_cap is None or free_cash_flow is None:
             return None
         return market_cap / free_cash_flow
 
-    # Current Liabilities
+    # Current Liabilities retrieval
     def get_current_liabilities(self):
-        return self._get_balance_sheet_value(['Total Current Liabilities'])
+        return self._get_balance_sheet_value(['Current Liabilities'])
 
-    # Total Liabilities
+    # Total Liabilities retrieval
     def get_total_liabilities(self):
-        return self._get_balance_sheet_value(['Total Liabilities Net Minority Interest'])
+        return self._get_balance_sheet_value(['Total Liabilities Net Minority Interest', 'Total Liabilities'])
 
-    # Current Assets
+    # Current Assets retrieval
     def get_current_assets(self):
-        return self._get_balance_sheet_value(['Total Current Assets'])
+        return self._get_balance_sheet_value(['Current Assets'])
 
-    # Total Stockholder Equity
+    # Total Stockholder Equity retrieval
     def get_total_stockholder_equity(self):
-        return self._get_balance_sheet_value(['Common Stock Equity', 'Total Stockholder Equity'])
+        return self._get_balance_sheet_value(['Common Stock Equity', 'Stockholders Equity'])
 
-    # Total Shares Outstanding
+    # Total Shares Outstanding retrieval
     def get_total_shares_outstanding(self):
         shares_outstanding = self.info.get('sharesOutstanding', None)
         if shares_outstanding is not None:
             return f"{shares_outstanding:,.0f}"  # Format with commas and no decimals
         return None
 
-    # Book Value per Share
+    # Book Value Per Share calculation
     def calculate_book_value_per_share(self):
         stockholder_equity = self.get_total_stockholder_equity()
         shares_outstanding = self.get_total_shares_outstanding()
-
         if stockholder_equity is None or shares_outstanding is None:
             return None
         return stockholder_equity / float(shares_outstanding.replace(",", ""))
 
-    # Payout Ratio
+    # Payout Ratio retrieval
     def calculate_payout_ratio(self):
         return self.info.get('payoutRatio', None)
 
-    # Beta (Volatility)
+    # Beta (Volatility) retrieval
     def get_beta(self):
         return self.info.get('beta', None)
 
-    # Institutional Ownership
+    # Institutional Ownership retrieval
     def get_institutional_ownership(self):
         return self.info.get('heldPercentInstitutions', None)
 
-    # Insider Buying/Selling
+    # Insider Buying/Selling retrieval
     def get_insider_transactions(self):
         return self.info.get('heldPercentInsiders', None)
 
-    # Asset Turnover Ratio
+    # Asset Turnover Ratio calculation
     def calculate_asset_turnover_ratio(self):
         total_assets = self._get_balance_sheet_value(['Total Assets'])
         revenue = self.get_revenue()
-
         if total_assets is None or revenue is None:
             return None
         return revenue / total_assets
 
-    # Helper functions for other metrics
+    # Retrieve total revenue
     def get_revenue(self):
-        """Retrieve total revenue."""
         return self._get_financials_value(['Total Revenue'])
 
+    # Retrieve operating income
     def get_operating_income(self):
-        """Retrieve operating income."""
         return self._get_financials_value(['Operating Income'])
 
+    # Retrieve interest expense
     def get_interest_expense(self):
-        """Retrieve interest expense."""
         return self._get_financials_value(['Interest Expense'])
 
+    # Retrieve book value of equity
     def get_book_value_of_equity(self):
-        """Retrieve book value of equity."""
         return self._get_balance_sheet_value(['Common Stock Equity'])
 
+    # Retrieve book value of debt
     def get_book_value_of_debt(self):
-        """Retrieve book value of debt."""
         return self._get_balance_sheet_value(['Total Debt'])
 
+    # Retrieve cash and equivalents
     def get_cash(self):
-        """Retrieve cash and equivalents."""
         return self._get_balance_sheet_value(['Cash And Cash Equivalents'])
 
+    # Retrieve short-term investments
     def get_short_term_investments(self):
-        """Retrieve short-term investments."""
         return self._get_balance_sheet_value(['Other Short Term Investments'])
 
+    # Effective Tax Rate calculation
     def calculate_effective_tax_rate(self):
-        """Calculate Effective Tax Rate."""
         income_before_tax = self._get_financials_value(['Income Before Tax'])
         tax_paid = self.cashflow.loc['Tax Paid'].iloc[0] if 'Tax Paid' in self.cashflow.index and not self.cashflow.loc['Tax Paid'].empty else None
-
         if income_before_tax is None or tax_paid is None:
             return None
         return tax_paid / income_before_tax
 
+    # Retrieve R&D expense
     def get_rnd_expense(self):
-        """Retrieve R&D expense."""
         return self._get_financials_value(['Research Development'])
